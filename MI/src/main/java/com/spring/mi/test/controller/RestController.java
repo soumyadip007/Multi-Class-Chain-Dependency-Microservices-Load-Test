b15d20858bbded741f64cfc38b8d4b956852232d
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-10 01:29:13 +0530

    rest

--- MI/src/main/java/com/spring/mi/test/controller/RestController.java
+++ MI/src/main/java/com/spring/mi/test/controller/RestController.java
@@ -6,8 +6,8 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.spring.mi.test.entity.E;
 import com.spring.mi.test.entity.S;
-import com.spring.mi.test.entity.V;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/rest")
@@ -35,6 +35,19 @@ public class RestController {
 		obj.add(new S("I'm 2nd S"));
 		obj.add(new S("I'm 3rd S"));
 		
+		S se=new S("Test EP");
+		E e=new E("Episode 1");
+		e.hm.put("101","HoiChoi");
+		e.hm.put("102","HoiChoi 2");
+		se.ep.add(e);
+		
+		S se1=new S("Test EP 1");
+		E e1=new E("Episode 11");
+		e1.hm.put("101","HoiChoi 11234");
+		e1.hm.put("102","HoiChoi 211234");
+		se1.ep.add(e1);
+		
+		
 		List<Object> list = new ArrayList <>();
 		list.add(integer);
 		list.add(string);
@@ -42,6 +55,8 @@ public class RestController {
 		for(S a:obj)
 			list.add(a);
 		
+		list.add(se);
+		list.add(se1);
 		
         return list;
     }
