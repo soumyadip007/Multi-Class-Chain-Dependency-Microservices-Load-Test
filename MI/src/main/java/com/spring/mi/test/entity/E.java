cce53bee8364bdc163c3f59ea131c18d88b4e3e1
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-10 00:06:45 +0530

    E

---
+++ MI/src/main/java/com/spring/mi/test/entity/E.java
@@ -0,0 +1,32 @@
+package com.spring.mi.test.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class E {
+
+	String id;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public E(String id) {
+		super();
+		this.id = id;
+	}
+	
+	
+}
