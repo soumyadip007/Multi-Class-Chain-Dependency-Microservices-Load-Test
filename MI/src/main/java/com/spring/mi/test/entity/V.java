09f661dd95995279ef5aafe7bb429fcfb138442f
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-09 23:55:33 +0530

    V

---
+++ MI/src/main/java/com/spring/mi/test/entity/V.java
@@ -0,0 +1,18 @@
+package com.spring.mi.test.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class V {
+
+	String id;
+	
+}
