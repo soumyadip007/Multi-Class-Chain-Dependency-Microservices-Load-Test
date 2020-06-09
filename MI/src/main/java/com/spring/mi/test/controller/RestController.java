034e4be8e6b0d3f7e0988be975439e4617918b1e
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-10 00:05:15 +0530

    Rest

--- MI/src/main/java/com/spring/mi/test/controller/RestController.java
+++ MI/src/main/java/com/spring/mi/test/controller/RestController.java
@@ -1,15 +1,48 @@
 package com.spring.mi.test.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.mi.test.entity.S;
+import com.spring.mi.test.entity.V;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/rest")
 public class RestController {
 
 	@GetMapping(value = "/server")
-    public String hello() {
+    public List<Object> hello() {
 		
-        return "Hi!";
+		List<Integer> integer = new ArrayList <Integer>();
+		List<String> string= new ArrayList <String>();
+		
+		integer.add(1);
+		integer.add(2);
+		integer.add(3);
+		integer.add(4);
+		
+		string.add("Soumyadip");
+		string.add("Soumya");
+		string.add("R");
+		string.add("R");
+		
+		
+		List<S> obj=new ArrayList<S>();
+		obj.add(new S("I'm 1st S"));
+		obj.add(new S("I'm 2nd S"));
+		obj.add(new S("I'm 3rd S"));
+		
+		List<Object> list = new ArrayList <>();
+		list.add(integer);
+		list.add(string);
+		
+		for(S a:obj)
+			list.add(a);
+		
+		
+        return list;
     }
 }
