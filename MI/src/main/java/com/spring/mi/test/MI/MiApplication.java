84d20f21cecc0f0d2ad95adcaa3562020567b062
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-09 23:48:37 +0530

    Init

---
+++ MI/src/main/java/com/spring/mi/test/MI/MiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mi.test.MI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiApplication.class, args);
+	}
+
+}
---
+++ MI/src/test/java/com/spring/mi/test/MI/MiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mi.test.MI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
