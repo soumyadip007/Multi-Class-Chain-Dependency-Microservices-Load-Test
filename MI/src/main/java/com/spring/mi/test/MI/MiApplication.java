d2319925eb571886b62adac4e3b605a8af28be27
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-09 23:50:42 +0530

    Git

--- MI/src/main/java/com/spring/mi/test/MI/MiApplication.java
+++ MI/src/main/java/com/spring/mi/test/MI/MiApplication.java
@@ -2,8 +2,14 @@ package com.spring.mi.test.MI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@ComponentScan({"com.spring.mi.test.*"})
+@EntityScan("com.spring.mi.test.*")
 public class MiApplication {
 
 	public static void main(String[] args) {
